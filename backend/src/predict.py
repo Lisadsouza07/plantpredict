43329ec69d28269cd7a9a78630df9030256c9b38
Lisa <lisadsouza102@gmail.com>
2026-06-15 13:30:01 -0600

    Updated predict.py

--- backend/src/predict.py
+++ backend/src/predict.py
@@ -1,8 +1,5 @@
 # goal : output disease prediction
 
-# leaf image -> preprocess image -> load trained model -> predict disease -> return class + confidence
-
-# imports
 import os
 import torch
 import json
@@ -14,39 +11,62 @@ from src.model import get_model
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 num_classes = 15
-model = get_model(num_classes)
 
 BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 
 MODEL_PATH = os.path.join(BASE_DIR, "models", "plant_disease_model.pth")
 CLASS_PATH = os.path.join(BASE_DIR, "models", "classes.json")
 
-model.load_state_dict(torch.load(MODEL_PATH, map_location=device))
-model = model.to(device)
-model.eval()
-
+# load class names once
 with open(CLASS_PATH, "r") as f:
     class_names = json.load(f)
+
 # image preprocessing
 transform = transforms.Compose([
     transforms.Resize((224, 224)),
     transforms.ToTensor(),
 ])
 
-# load + preprocess image
 def load_image(image_path):
     image = Image.open(image_path).convert("RGB")
     image = transform(image)
     image = image.unsqueeze(0)
     return image.to(device)
 
+
+# ----------------------------
+# MODEL LOADING (SINGLETON)
+# ----------------------------
+model = None
+
+def load_model_once():
+    global model
+    if model is None:
+        print("Loading model for first time...")
+
+        model = get_model(num_classes)
+        model.load_state_dict(
+            torch.load(MODEL_PATH, map_location=device)
+        )
+        model.to(device)
+        model.eval()
+
+    return model
+
+
+# ----------------------------
+# PREDICTION FUNCTION
+# ----------------------------
 def predict(image_path):
+    model = load_model_once()
+
     image = load_image(image_path)
 
     with torch.no_grad():
         outputs = model(image)
         probabilities = F.softmax(outputs, dim=1)
         top_probs, top_classes = torch.topk(probabilities, 3)
+
     predictions = []
 
     for prob, cls in zip(top_probs[0], top_classes[0]):
